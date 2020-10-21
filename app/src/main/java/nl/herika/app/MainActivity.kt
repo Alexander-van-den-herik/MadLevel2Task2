5ee8191aced1242d3d4088da65be6ddc86d13d74
Alexander van den Herik <alexander.van.den.herik2@hva.nl>
2020-10-21 11:42:13 +0200

    Questions can now be swipped to check if the answer is correct

--- app/src/main/java/nl/herika/app/MainActivity.kt
+++ app/src/main/java/nl/herika/app/MainActivity.kt
@@ -3,8 +3,10 @@ package nl.herika.app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import nl.herika.app.data.Question
 import nl.herika.app.databinding.ActivityMainBinding
 
@@ -40,6 +42,52 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
+        for (i in Question.QUESTIONS.indices) {
+            questions.add(Question(Question.QUESTIONS[i], Question.ANSWERS[i]))
+        }
 
+        createItemTouchHelper().attachToRecyclerView(binding.rvQuestions)
+
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        val callback = object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewholder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                if (questions[position].answer != direction) {
+                    Snackbar.make(
+                        binding.rvQuestions.getChildAt(position),
+                        "That's the wrong answer, please try again!",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                } else {
+
+                    Snackbar.make(
+                        binding.rvQuestions.getChildAt(position),
+                        "Congratulations, that's the correct answer!",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+
+                questionAdapter.notifyDataSetChanged()
+            }
+
+        }
+
+        return ItemTouchHelper(callback)
     }
 }
