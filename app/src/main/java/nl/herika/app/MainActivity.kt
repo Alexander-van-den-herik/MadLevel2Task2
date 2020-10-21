f8731a23f0d010b49a3ca77033198a1c30bc86a0
Alexander van den Herik <alexander.van.den.herik2@hva.nl>
2020-10-21 11:28:28 +0200

    Created view InitView

--- app/src/main/java/nl/herika/app/MainActivity.kt
+++ app/src/main/java/nl/herika/app/MainActivity.kt
@@ -1,12 +1,45 @@
 package nl.herika.app
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import nl.herika.app.data.Question
+import nl.herika.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val questions = arrayListOf<Question>()
+    private val questionAdapter = QuestionAdapter(questions)
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.rvQuestions.layoutManager = LinearLayoutManager(
+            this@MainActivity,
+            RecyclerView.VERTICAL,
+            false
+        )
+
+        binding.rvQuestions.adapter = questionAdapter
+
+        binding.rvQuestions.addItemDecoration(
+            DividerItemDecoration(
+                this@MainActivity,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
+
     }
 }
